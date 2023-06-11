850a3022455b54b416f741d798e8e9455afe661d
DatDuong <thanhdatduong24102001@gmail.com>
2023-06-11 17:29:46 +0700

    update cal function

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Xin chao toi la dat");
+        System.out.println("Xin chao toi la thanh dat");
     }
 }
--- src/App2.java
+++ src/App2.java
@@ -1,5 +1,6 @@
 public class App2 {
     public static void main(String[] args) {
         System.out.println("Day la demo 2");
+        System.out.println(5+6);
     }
 }
