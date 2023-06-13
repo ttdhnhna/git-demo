c68d272ef838a88ac9593b70d56fd1115018623f
DatDuong <thanhdatduong24102001@gmail.com>
2023-06-13 17:33:11 +0700

    update App2.java

--- src/App2.java
+++ src/App2.java
@@ -1,5 +1,8 @@
 public class App2 {
     public static void main(String[] args) {
         System.out.println("Day la demo 2");
+        int a=2;
+        int b=3;
+        System.out.println(a+b);
     }
 }
